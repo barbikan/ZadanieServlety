329254cae183d28be4371e1bc482c74013684f87
Uzytkownik <Uzytkownik@kursGIT.pl>
2020-07-19 11:20:49 +0200

    added HElloServlet/random message

--- src/main/java/com/sda/AboutMeServlet.java
+++ src/main/java/com/sda/AboutMeServlet.java
@@ -1,4 +1,21 @@
+package com.sda;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class AboutMeServlet extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
 
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+
+                writer.println("<p>Hello, World</p>"); //tu można pisać cały html - wiele linijek
+    }
 }
--- src/main/java/com/sda/ProgrammingLanguageDashboardServlet.java
+++ src/main/java/com/sda/ProgrammingLanguageDashboardServlet.java
@@ -1,2 +1,4 @@
+package com.sda;
+
 public class ProgrammingLanguageDashboardServlet {
 }
